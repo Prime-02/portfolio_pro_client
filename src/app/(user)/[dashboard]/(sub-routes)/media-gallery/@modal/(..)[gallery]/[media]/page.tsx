d363d036fafcfea62a2d5e2db9db89117d18ba73
Prime-02 <163609502+Prime-02@users.noreply.github.com>
2025-08-16 19:11:31 +0100

    ...

--- src/app/(user)/[dashboard]/(sub-routes)/media-gallery/@modal/(..)[gallery]/[media]/page.tsx
+++ src/app/(user)/[dashboard]/(sub-routes)/media-gallery/@modal/(..)[gallery]/[media]/page.tsx
@@ -1,9 +1,11 @@
-import React from 'react'
-
+// In your modal page.tsx
 const page = () => {
+  console.log('MODAL RENDERING!');
   return (
-    <div>Image modal </div>
-  )
+    <div style={{position: 'fixed', top: 0, left: 0, right: 0, bottom: 0, background: 'rgba(0,0,0,0.8)', zIndex: 9999}}>
+      <div style={{background: 'white', margin: '50px', padding: '20px'}}>
+        Image modal
+      </div>
+    </div>
+  );
 }
-
-export default page

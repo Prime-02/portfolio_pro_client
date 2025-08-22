69cffd4c1e8602653abeedb4b20d8a554be485f4
Prime-02 <163609502+Prime-02@users.noreply.github.com>
2025-08-22 20:56:58 +0100

    Update urls.ts

--- src/app/components/utilities/indices/urls.ts
+++ src/app/components/utilities/indices/urls.ts
@@ -1,3 +1,3 @@
-export const BASE_URL = `http://portfolio-pro-docker-env.eba-my2ee9av.us-west-2.elasticbeanstalk.com`;
+export const BASE_URL = `https://d22qn4oxzh4x4y.cloudfront.net`;
 
 export const V1_BASE_URL = `${BASE_URL}/api/v1`;

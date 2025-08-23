c11c35e44a939e637b4f93995171810ebfeeba7e
Prime-02 <163609502+Prime-02@users.noreply.github.com>
2025-08-23 17:37:16 +0100

    Update urls.ts

--- src/app/components/utilities/indices/urls.ts
+++ src/app/components/utilities/indices/urls.ts
@@ -2,7 +2,7 @@ export const local = "https://3c5334cde5b1.ngrok-free.app";
 
 export const cloud = `https://d22qn4oxzh4x4y.cloudfront.net`;
 
-export const BASE_URL = local;
+export const BASE_URL = cloud;
 
 export const V1_BASE_URL = `${BASE_URL}/api/v1`;
 

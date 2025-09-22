a74693435fbd023fa529077729b86fb3dcacef93
Prime-02 <163609502+Prime-02@users.noreply.github.com>
2025-09-22 18:13:23 +0100

    Update urls.ts

--- src/app/components/utilities/indices/urls.ts
+++ src/app/components/utilities/indices/urls.ts
@@ -2,7 +2,7 @@ export const local = "http://localhost:8000";
 
 export const cloud = `https://d22qn4oxzh4x4y.cloudfront.net`;
 
-export const BASE_URL = local;
+export const BASE_URL = cloud;
 
 export const V1_BASE_URL = `${BASE_URL}/api/v1`;
 

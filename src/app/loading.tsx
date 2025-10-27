5ceaa46205ac558208a4ab054a6fa31d9d7495fe
Utojiuba Chidera <utojiubachidera2@gmail.com>
2025-10-27 10:05:36 +0100

    Update loading.tsx

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -4,12 +4,13 @@ import PortfolioProLogo from "./components/logo/PortfolioProTextLogo";
 export default function Loading() {
   return (
     <div className="flex justify-center items-center h-screen">
-      <PortfolioProLogo
+      {/* <PortfolioProLogo
         tracking={0.2}
         scale={0.75}
         fontWeight={"extrabold"}
         reanimateDelay={3000}
-      />{" "}
+      />{" "} */}
+      loading...
     </div>
   );
 }

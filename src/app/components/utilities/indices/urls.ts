76991b74fee339defdeb8c86851a79cde6f22bb4
Utojiuba Chidera <163609502+Prime-02@users.noreply.github.com>
2026-03-01 21:28:21 +0100

    Update urls.ts

--- src/app/components/utilities/indices/urls.ts
+++ src/app/components/utilities/indices/urls.ts
@@ -1,7 +1,7 @@
 //WEBSOCKET
 export const wsLocal = "ws://localhost:8000";
 
-export const wsCloud = `wss://d22qn4oxzh4x4y.cloudfront.net`;
+export const wsCloud = `wss://portfolio-pro-server.onrender.com`;
 
 export const WS_BASE_URL = wsLocal;
 
@@ -15,7 +15,7 @@ export const WS_V2_BASE_URL = `${WS_BASE_URL}/api/v2`;
 //HTTP
 export const local = "http://localhost:8000";
 
-export const cloud = `https://d22qn4oxzh4x4y.cloudfront.net`;
+export const cloud = `https://portfolio-pro-server.onrender.com`;
 
 export const BASE_URL = local;
 

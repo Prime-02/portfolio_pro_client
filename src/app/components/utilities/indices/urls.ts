dcf2a26628a619b4f4482c18fc68a4e60810699d
Utojiuba Chidera <163609502+Prime-02@users.noreply.github.com>
2026-03-30 22:14:24 +0100

    Update WebSocket and HTTP cloud URLs

--- src/app/components/utilities/indices/urls.ts
+++ src/app/components/utilities/indices/urls.ts
@@ -1,9 +1,9 @@
 //WEBSOCKET
 export const wsLocal = "ws://localhost:8000";
 
-export const wsCloud = `wss://portfolio-pro-server.onrender.com`;
+export const wsCloud = `ws://portfolio-pro-server.fastapicloud.dev`;
 
-export const WS_BASE_URL = wsLocal;
+export const WS_BASE_URL = wsCloud;
 
 export const WS_V1_BASE_URL = `${WS_BASE_URL}/api/v1`;
 
@@ -15,7 +15,7 @@ export const WS_V2_BASE_URL = `${WS_BASE_URL}/api/v2`;
 //HTTP
 export const local = "http://localhost:8000";
 
-export const cloud = `https://portfolio-pro-server.onrender.com`;
+export const cloud = `https://portfolio-pro-server.fastapicloud.dev`;
 
 export const BASE_URL = cloud;
 
